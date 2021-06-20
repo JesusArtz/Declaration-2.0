09d33a725542ca95c9d63e19c8ccd24d7201286c
JesusArtz <85129357+JesusArtz@users.noreply.github.com>
2021-06-20 00:15:40 -0500

    Add files via upload

---
+++ js/Alertas.js
@@ -0,0 +1,10 @@
+Swal.fire({
+    title: 'Estas ante la declaracion mas epica de tu vida <3',
+    showClass: {
+      popup: 'animate__animated animate__fadeInDown'
+    },
+    hideClass: {
+      popup: 'animate__animated animate__fadeOutUp'
+    }
+  });
+
